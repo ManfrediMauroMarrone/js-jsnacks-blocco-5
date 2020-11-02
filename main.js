a7f6be43d41862b16b065c55fcdeacf446aa1f01
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-02 16:01:55 +0100

    creato array oggetti

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var bikes = [
+    {
+      nome : 'pippo',
+      peso : '7'
+    },
+
+    {
+      nome : 'pluto',
+      peso : '8'
+    },
+
+    {
+      nome : 'paperino',
+      peso : '5'
+    },
+
+    {
+      nome : 'franco',
+      peso : '9'
+    }
+  ]
+
+  console.log(bikes);
+
+
+
+
+})
