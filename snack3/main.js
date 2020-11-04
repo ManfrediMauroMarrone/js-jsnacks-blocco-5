324caaefcf68db286bbd13855ce4ba3d54d5e1ee
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-04 15:36:33 +0100

    modifiche finali

---
+++ snack3/main.js
@@ -0,0 +1,31 @@
+// JSnack2: Creare un array di nomi e chiedere all'utente due numeri.
+// Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
+
+// creo un array di nomi
+$(document).ready(function() {
+  var nomi = ['pippo', 'pluto', 'paperino', 'topolino', 'paperone', 'minnie', 'paperina', 'qui', 'quo', 'qua']
+
+  console.log(nomi);
+  // chiedo all'utente 2 numeri
+
+  var inizio = parseInt(prompt('inserisci il primo numero'))
+
+  var fine = parseInt(prompt('inserisci il secondo numero'))
+
+  // devo creare un array con in nomi nelle posizioni comprese tra i numeri che mi da l'utente
+
+  var nomiEstratti = [];
+
+  for (var i = inizio; i <= fine; i++) {
+    var nome = nomi[i]
+    console.log(nome);
+    nomiEstratti.push(nome);
+  }
+
+  console.log(nomiEstratti);
+
+  // var selected = names.slice(inizio, fine+1)
+  //
+  // console.log(selected);
+
+})
